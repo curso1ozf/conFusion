0a9b94fa5969a7fa63bc2a8733c27ce68f69f5b2
Oscar Zarraluqui <oscarzarraluqui@MacBook-Pro-de-Oscar.local>
2018-07-08 15:12:43 +0200

    angular components part2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 //import { MaterialModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -21,6 +25,9 @@ import { MenuComponent } from './menu/menu.component';
     //MaterialModule,
     MatListModule,
     MatToolbarModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
   ],
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,25 +1,88 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-
+const DISHES: Dish[] =[
+  {
+    name: 'Uthappizza',
+    image: '/assets/images/uthappizza.png',
+    category: 'mains',
+    label: 'Hot',
+    price: '4.99',
+    // tslint:disable-next-line:max-line-length
+    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
+  },
+  {
+    name: 'Zucchipakoda',
+    image: '/assets/images/zucchipakoda.png',
+    category: 'appetizer',
+    label: '',
+    price: '1.99',
+    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
+  },
+  {
+    name: 'Vadonut',
+    image: '/assets/images/vadonut.png',
+    category: 'appetizer',
+    label: 'New',
+    price: '1.99',
+    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
+  },
+  {
+    name: 'ElaiCheese Cake',
+    image: '/assets/images/elaicheesecake.png',
+    category: 'dessert',
+    label: '',
+    price: '2.99',
+    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+  }
+];
 
 @Component({
   selector: 'app-menu',
   template: `
     <div class="container" fxLayout="column" fxLayoutGap="10px">
-      <mat-list fxFlex>
-        <mat-list-item *ngFor="let dish of dishes">
-          <img matListAvatar src={{dish.image}} alt={{dish.name}}>
-          <h1 matLine> {{dish.name}} </h1>
-          <p matLine> 
-            <span>{{dish.description}} </span>
-          </p>
-        </mat-list-item>
-      </mat-list>
+      <div fxFlex>
+        <div>
+          <h3>Menu</h3>
+          <hr>
+        </div>
+      </div>  
+    
+    <div fxFlex>
+      <mat-grid-list cols="2" rowHeight="200px">
+        <mat-grid-tile *ngFor="let dish of dishes">
+          <img height="200px" src={{dish.image}} alt={{dish.name}}>
+          <mat-grid-tile-footer>
+            <h1 matLine>{{dish.name | uppercase}}</h1>
+          </mat-grid-tile-footer>
+        </mat-grid-tile>
+      </mat-grid-list>
     </div>
+
+    <div fxFlex *ngIf="selectedDish">
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>
+          <h3>{{selectedDish.name | uppercase}}</h3>
+        </mat-card-title>
+      </mat-card-header>
+      <img mat-card-image src={{selectedDish.image}} alt={{selectedDish.name}}>
+      <mat-card-content>
+        <p>{{selectedDish.description}}
+        </p>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-button>LIKE</button>
+        <button mat-button>SHARE</button>
+      </mat-card-actions>
+    </mat-card>
+  </div>
   `,
   styles: []
 })
+
+
+
 export class MenuComponent implements OnInit {
 
   constructor() { }
@@ -27,40 +90,8 @@ export class MenuComponent implements OnInit {
   ngOnInit() {
   }
 
-  dishes: Dish[] = [
-    {
-      name: 'Uthappizza',
-      image: '/assets/images/uthappizza.png',
-      category: 'mains',
-      label: 'Hot',
-      price: '4.99',
-      // tslint:disable-next-line:max-line-length
-      description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-    },
-    {
-      name: 'Zucchipakoda',
-      image: '/assets/images/zucchipakoda.png',
-      category: 'appetizer',
-      label: '',
-      price: '1.99',
-      description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-    },
-    {
-      name: 'Vadonut',
-      image: '/assets/images/vadonut.png',
-      category: 'appetizer',
-      label: 'New',
-      price: '1.99',
-      description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
-    },
-    {
-      name: 'ElaiCheese Cake',
-      image: '/assets/images/elaicheesecake.png',
-      category: 'dessert',
-      label: '',
-      price: '2.99',
-      description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-    }
-   ];
+  dishes: Dish[] = DISHES;
+
+  selectedDish = DISHES[0];
 
 }
